d3052d88b434f746bce2d912a945afb830889043
Jared Kipe <jared@hibou.io>
2016-10-12 17:54:04 -0700

    Migrate to v10

--- partner_gravatar.py
+++ partner_gravatar.py
@@ -2,7 +2,7 @@ import urllib2
 import urllib
 import hashlib
 
-from openerp import models, api, tools
+from odoo import models, api, tools
 
 def download_gravatar(email, icon_set):
     gravatar_url = 'http://www.gravatar.com/avatar/' + \
@@ -17,17 +17,11 @@ class GravatarPartner(models.Model):
 
     # Override
     @api.model
-    def _get_default_image(self, is_company, colorize=False):
-        return False
-
-    # Override
-    @api.model
     def create(self, values):
-        new_partner = super(GravatarPartner, self).create(values)
-        if not bool(new_partner.image):
-            new_partner._gravatar_update_image()
+        if bool(values.get('email')) and not bool(values.get('image')):
+            values['image'] = self._fetch_and_process_gravatar(values.get('email'), values.get('is_company'))
 
-        return new_partner
+        return super(GravatarPartner, self).create(values)
 
     @api.multi
     @api.onchange('email')
@@ -41,19 +35,33 @@ class GravatarPartner(models.Model):
         for p in self.filtered(lambda r: not bool(r.image)):
             p._gravatar_update_image()
 
-    def _gravatar_update_image(self):
-        if bool(self.email):
-            gravatar_set = self._load_icon_config()
-            image = download_gravatar(self.email, gravatar_set)
-            if image:
-                self.image = tools.image_resize_image_big(image.encode('base64'))
-        else:
-            self.image = super(GravatarPartner, self)._get_default_image(bool(self.is_company), True)
 
-    def _load_icon_config(self):
+    def _gravatar_update_image(self):
+        """
+        single record update
+        """
+        if bool(self.email):
+            self.image = self._fetch_and_process_gravatar(self.email, self.is_company)
+        else:
+            self.image = super(GravatarPartner, self)._get_default_image(self.type, self.is_company, self.parent_id)
+
+    def _fetch_and_process_gravatar(self, email, is_company):
+        """
+        Functional hook to return a properly 'odoo' formatted image
+        :param email: string
+        :param is_company: boolean
+        :return: image suitable for attachment
+        """
+        gravatar_set = self._load_icon_config(is_company)
+        image = download_gravatar(email, gravatar_set)
+        if image:
+            return tools.image_resize_image_big(image.encode('base64'))
+        return False
+
+    def _load_icon_config(self, is_company):
         config = self.env['ir.config_parameter'].sudo()
         key = 'gravatar.icon_set'
-        if self.is_company:
+        if is_company:
             key += '.company'
 
         gravatar_config = config.search([('key', '=', key)])
